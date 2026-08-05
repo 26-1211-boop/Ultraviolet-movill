24bb9d27ac2ed8e78cd1cbfbe66d8a20500107b5
26-1211-boop <26-1211@sahmyook.hs.kr>
2026-08-06 08:52:37 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,8 +1,26 @@
 self.__uv$config = {
     prefix: '/uv/service/',
     bare: 'https://bare.benrogo.net/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: function(url) {
+        if (!url) return url;
+        return encodeURIComponent(
+            url
+                .toString()
+                .split('')
+                .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char))
+                .join('')
+        );
+    },
+    decodeUrl: function(url) {
+        if (!url) return url;
+        const [input, ...query] = url.split('?');
+        return (
+            decodeURIComponent(input)
+                .split('')
+                .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char))
+                .join('') + (query.length ? '?' + query.join('?') : '')
+        );
+    },
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
