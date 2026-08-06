55d44389fd8706e37de3da7c622bfc2c8fea753a
26-1211-boop <26-1211@sahmyook.hs.kr>
2026-08-06 13:26:59 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,11 @@
 self.__uv$config = {
-    prefix: '/uv/service/',
-    bare: 'https://titanium-network--26-1211.replit.app/bare/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+  prefix: "/uv/service/",
+  bare: "https://bare.benrogo.net/", // 공개 Bare 백엔드 서버 주소 등록
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
