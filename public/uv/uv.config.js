b8273e3b3bbecebb48f51b42a17ba47770379b26
26-1211-boop <26-1211@sahmyook.hs.kr>
2026-08-06 13:27:45 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://bare.benrogo.net/", // 공개 Bare 백엔드 서버 주소 등록
+  bare: "https://bare.benrogo.net/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
