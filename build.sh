e09763288ec88e3c6733b1ec352a370e71bf2d08
26-1211-boop <26-1211@sahmyook.hs.kr>
2026-08-06 09:55:52 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+npm install @titaniumnetwork-dev/ultraviolet @mercuryworkshop/bare-mux @mercuryworkshop/epoxy-tls
 mkdir -p public/uv public/bare-mux public/epoxy
-cp -r node_modules/@titaniumnetwork-dev/ultraviolet/dist/* public/uv/ || true
-cp -r node_modules/@mercuryworkshop/bare-mux/dist/* public/bare-mux/ || true
-cp -r node_modules/@mercuryworkshop/epoxy-tls/build/* public/epoxy/ || true
+cp -r node_modules/@titaniumnetwork-dev/ultraviolet/dist/* public/uv/
+cp -r node_modules/@mercuryworkshop/bare-mux/dist/* public/bare-mux/
+cp -r node_modules/@mercuryworkshop/epoxy-tls/build/* public/epoxy/
