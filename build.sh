11879f0d6935c329a88bb7870b552f3664841165
26-1211-boop <26-1211@sahmyook.hs.kr>
2026-08-06 09:34:26 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+mkdir -p public/uv public/bare-mux public/epoxy
+cp -r node_modules/@titaniumnetwork-dev/ultraviolet/dist/* public/uv/
+cp -r node_modules/@mercuryworkshop/bare-mux/dist/* public/bare-mux/
+cp -r node_modules/@mercuryworkshop/epoxy-tls/build/* public/epoxy/
